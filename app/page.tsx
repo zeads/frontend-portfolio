87d045271d5b52441a94b6911201d5eec8230c9b
zeads <ruri.pelinandang@gmail.com>
2026-01-26 10:00:08 +0700

    test: edit check deploy edgeone pages

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,20 @@ export default function Home() {
   return (
     <>
       <h1>Hello World</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem
+        assumenda distinctio ullam atque aliquid dolor vel magnam quasi
+        cupiditate porro commodi ex corporis velit nulla officiis ducimus
+        doloribus fugiat nesciunt eius animi tempora, libero recusandae
+        doloremque dolorum. Voluptatibus nulla necessitatibus incidunt ipsa,
+        delectus sit maiores possimus sed cum exercitationem nihil cumque quod
+        perferendis odit fuga magni nobis omnis ducimus pariatur. Eum aperiam
+        expedita nam quisquam exercitationem aliquid voluptate temporibus
+        voluptates, laboriosam nostrum consequuntur placeat odit doloribus minus
+        molestiae ab vel cum odio voluptatem autem et. Sit obcaecati minus id
+        dolorum itaque corporis veritatis quibusdam nobis, repellendus tenetur,
+        optio reprehenderit assumenda.
+      </p>
     </>
   );
 }
