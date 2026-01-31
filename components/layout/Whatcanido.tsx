24f8678bd5bdb94f871c5bce50aae43601dd4a3d
zeads <ruri.pelinandang@gmail.com>
2026-01-31 07:41:51 +0700

    feat: motion in what i can do section

--- components/layout/Whatcanido.tsx
+++ components/layout/Whatcanido.tsx
@@ -7,6 +7,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import * as motion from "motion/react-client";
 
 export default function Whatcanido() {
   return (
@@ -15,89 +16,137 @@ export default function Whatcanido() {
       className=" lg:h-full items-center justify-between  px-10 pt-20"
     >
       <div className="text-center">
-        <h1 className="font-bold text-sm text-blue-500">SERVICES</h1>
-        <h3 className="font-bold text-2xl">What I Can Do For You</h3>
+        <motion.h1
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+          viewport={{ once: true }}
+          className="font-bold text-sm text-blue-500"
+        >
+          SERVICES
+        </motion.h1>
+        <motion.h3
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.3 }}
+          viewport={{ once: true }}
+          className="font-bold text-2xl"
+        >
+          What I Can Do For You
+        </motion.h3>
       </div>
-      <div className="flex flex-wrap mt-2 gap-10 lg:px-30">
-        <Card className="mx-auto w-full max-w-sm">
-          <CardHeader>
-            <CardTitle>Frontend Development</CardTitle>
-            <CardDescription>
-              Tech: React, Next.js, Tailwind CSS, TypeScript.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>
-              Crafting fast, responsive, and SEO-friendly user interfaces. Using
-              Next.js and React, I turn designs into interactive reality with a
-              focus on performance and accessibility.
-            </p>
-          </CardContent>
-          <CardFooter>
-            <Button variant="outline" size="sm" className="w-full">
-              &quot;Interested? Let&apos;s discuss your project.&quot;
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card className="mx-auto w-full max-w-sm">
-          <CardHeader>
-            <CardTitle>Backend Architecture</CardTitle>
-            <CardDescription>
-              Tech: Node.js, Express, JWT, TypeScript.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>
-              Building the &quot;brains&quot; of your application. I develop
-              secure and scalable server-side logic and RESTful APIs using
-              Express.js to ensure your data flows seamlessly.
-            </p>
-          </CardContent>
-          <CardFooter>
-            <Button variant="outline" size="sm" className="w-full">
-              &quot;Interested? Let&apos;s discuss your project.&quot;
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card className="mx-auto w-full max-w-sm">
-          <CardHeader>
-            <CardTitle>Full-Stack Solutions</CardTitle>
-            <CardDescription>Tech: The Full MERN/PERN Stack.</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>
-              The complete package. I take your idea from a blank page to a
-              fully functioning web application, handling everything from the
-              database schema to the final deployment.
-            </p>
-          </CardContent>
-          <CardFooter>
-            <Button variant="outline" size="sm" className="w-full">
-              &quot;Interested? Let&apos;s discuss your project.&quot;
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card className="mx-auto w-full max-w-sm">
-          <CardHeader>
-            <CardTitle>Database Design & Management</CardTitle>
-            <CardDescription>
-              Tech: PostgreSQL, MongoDB, Prisma, Mongoose.
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <p>
-              Designing efficient data structures for your needs. Whether
-              it&apos;s the relational integrity of PostgreSQL or the flexible
-              document-based approach of MongoDB, I ensure your data is safe and
-              optimized.
-            </p>
-          </CardContent>
-          <CardFooter>
-            <Button variant="outline" size="sm" className="w-full">
-              &quot;Interested? Let&apos;s discuss your project.&quot;
-            </Button>
-          </CardFooter>
-        </Card>
+      <div className="flex flex-wrap items-center justify-center mt-2 gap-10 lg:px-30">
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.6 }}
+          viewport={{ once: true }}
+          className="mx-auto w-full max-w-sm"
+        >
+          <Card>
+            <CardHeader>
+              <CardTitle>Frontend Development</CardTitle>
+              <CardDescription>
+                Tech: React, Next.js, Tailwind CSS, TypeScript.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>
+                Crafting fast, responsive, and SEO-friendly user interfaces.
+                Using Next.js and React, I turn designs into interactive reality
+                with a focus on performance and accessibility.
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button variant="outline" size="sm" className="w-full">
+                &quot;Interested? Let&apos;s discuss your project.&quot;
+              </Button>
+            </CardFooter>
+          </Card>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.6 }}
+          viewport={{ once: true }}
+          className="mx-auto w-full max-w-sm"
+        >
+          <Card>
+            <CardHeader>
+              <CardTitle>Backend Architecture</CardTitle>
+              <CardDescription>
+                Tech: Node.js, Express, JWT, TypeScript.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>
+                Building the &quot;brains&quot; of your application. I develop
+                secure and scalable server-side logic and RESTful APIs using
+                Express.js to ensure your data flows seamlessly.
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button variant="outline" size="sm" className="w-full">
+                &quot;Interested? Let&apos;s discuss your project.&quot;
+              </Button>
+            </CardFooter>
+          </Card>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.9 }}
+          viewport={{ once: true }}
+          className="mx-auto w-full max-w-sm"
+        >
+          <Card>
+            <CardHeader>
+              <CardTitle>Full-Stack Solutions</CardTitle>
+              <CardDescription>Tech: The Full MERN/PERN Stack.</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>
+                The complete package. I take your idea from a blank page to a
+                fully functioning web application, handling everything from the
+                database schema to the final deployment.
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button variant="outline" size="sm" className="w-full">
+                &quot;Interested? Let&apos;s discuss your project.&quot;
+              </Button>
+            </CardFooter>
+          </Card>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8, delay: 0.9 }}
+          viewport={{ once: true }}
+          className="mx-auto w-full max-w-sm"
+        >
+          <Card>
+            <CardHeader>
+              <CardTitle>Database Design & Management</CardTitle>
+              <CardDescription>
+                Tech: PostgreSQL, MongoDB, Prisma, Mongoose.
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <p>
+                Designing efficient data structures for your needs. Whether
+                it&apos;s the relational integrity of PostgreSQL or the flexible
+                document-based approach of MongoDB, I ensure your data is safe
+                and optimized.
+              </p>
+            </CardContent>
+            <CardFooter>
+              <Button variant="outline" size="sm" className="w-full">
+                &quot;Interested? Let&apos;s discuss your project.&quot;
+              </Button>
+            </CardFooter>
+          </Card>
+        </motion.div>
       </div>
     </section>
   );
